1874c7f11331a5e426039b1691ace2ea06c576a6
Michael Gorsuch <gorsuch@simple.com>
2014-06-01 11:44:52 -0500

    allow for a configurable DOCKER_OPTS

--- spec/default/docker_spec.rb
+++ spec/default/docker_spec.rb
@@ -8,3 +8,8 @@ describe service('docker') do
   it { should be_enabled   }
   it { should be_running   }
 end
+
+describe file('/etc/default/docker') do
+  it { should be_file }
+  its(:content) { should match /^DOCKER_OPTS=""/ }
+end
